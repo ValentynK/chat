4a82ba09a81a1105a3f5336d4780ed61eda88797
Valentyn <v.karkavchuk@gmail.com>
2015-05-05 19:13:16 +0300

    First commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new
+    @comment = Comment.new
+    @comments = Comment.order('created_at DESC')
+  end
+
+  def create
+    respond_to do |format|
+      if current_user
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+          flash.now[:notice] = 'Your comment was successfully posted!'
+        else
+          flash.now[:error] = 'Your comment cannot be saved.'
+        end
+        format.html {redirect_to root_url}
+        format.js
+      else
+        format.html{redirect_to root_url}
+        format.js {render nothing: true}
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
